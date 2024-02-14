a4739d1f9c70e5e164232610369d75066cd730c9
Emmanuel <emmanuelcalad@gmail.com>
2024-02-14 13:47:25 -0500

    Add functions

--- Figuras/figuras.py
+++ Figuras/figuras.py
@@ -1,12 +1,33 @@
+import math
+
 
 class Punto:
-
-    def __int__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
+    def __init__(self, x: int, y: int):
+        self.x: int = x
+        self.y: int = y
 
 
 class Circulo:
-    pass
 
+    def __init__(self, radio: int, centro: Punto):
+        self.radio: int = radio
+        self.centro: Punto = centro
+
+
+    def calcular_area(self) -> float:
+
+        return math.pi * self.radio ** 2
+
+    def ubicacion_punto(self, punto : Punto):
+
+        punto = punto
+        if self.centro.x > punto.x and self.centro.y > punto.y:
+            return print("El punto se encuentra dentro del circulo")
+
+        else:
+            return print("El punto está por fuera del circulo")
+
+    def contiene(self, punto:Punto) -> bool:
+        return (punto.x-self.centro.x)**2+(punto.y-self.centro.y)**2 <= self.radio**2
+
+#Realizar función que diga si dos circulos se interceptan
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from Figuras.figuras import Circulo, Punto
+
+punto_1: Punto = Punto(2,3)
+circulo_1: Circulo = Circulo(3, Punto(2,3))
+
+print(circulo_1.calcular_area())
+
+print(circulo_1.ubicacion_punto(punto_1))
+
+print(circulo_1.contiene(punto_1))
+
+
 
